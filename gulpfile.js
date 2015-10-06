ecc05e5bab9eec0cbbd37ae5464c81f5cdff9962
David Lemayian <david@on.co.ke>
2015-10-06 12:29:23 +0300

    Added font-awesome dependency local.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
-  // place code for your default task here
+
   gulp.src('bower_components/bootstrap-sass/assets/stylesheets/**')
-      .pipe(gulp.dest('_sass'))
+      .pipe(gulp.dest('_sass'));
+
+  gulp.src('bower_components/bootstrap-sass/assets/**')
+      .pipe(gulp.dest('static'));
+
+  gulp.src('bower_components/jquery/dist/**')
+      .pipe(gulp.dest('static/js'));
+
+  gulp.src('bower_components/font-awesome/css/**')
+      .pipe(gulp.dest('static/css'));
+  gulp.src('bower_components/font-awesome/fonts/**')
+      .pipe(gulp.dest('static/fonts'));
+
 });
